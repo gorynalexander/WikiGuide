013e0a6c056cdd3787684f50b6996ab230b443e5
goryn.alexander <goryn.alexander@gmail.com>
2017-09-29 02:29:54 +0300

    Обновление перфоманса

--- app/src/main/java/com/goryn/wikiguide/ui/MainActivity.java
+++ app/src/main/java/com/goryn/wikiguide/ui/MainActivity.java
@@ -2,48 +2,50 @@ package com.goryn.wikiguide.ui;
 
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.net.ConnectivityManager;
+import android.os.Build;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.location.Location;
-import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+
+import android.support.annotation.NonNull;
+
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+
+
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.SettingsApi;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.gson.Gson;
 import com.goryn.wikiguide.App;
 import com.goryn.wikiguide.R;
-import com.goryn.wikiguide.model.Page;
 import com.goryn.wikiguide.model.QueryResult;
 import com.goryn.wikiguide.ui.fragments.GameMapFragment;
 import com.goryn.wikiguide.ui.fragments.PlacesFragment;
 import com.goryn.wikiguide.utils.NetworkBroadcastReceiver;
 import com.goryn.wikiguide.utils.WikiQueryService;
 
-import java.util.List;
 import java.util.Locale;
 
 import retrofit2.Call;
@@ -51,10 +53,13 @@ import retrofit2.GsonConverterFactory;
 import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
-    Toolbar toolbar;
 
     FragmentManager fragmentManager;
     Fragment fragment;
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
 
     /*  Location   */
     private GoogleApiClient googleApiClient;
@@ -72,9 +77,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         initNavDrawer();
 
         buildGoogleAPiClient();
@@ -83,6 +85,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         broadcastReceiver.setActivityHandler(this);
         registerReceiver(broadcastReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
         // TODO : UNREGISTER BROADCAST RECIEVING
+
+        if (savedInstanceState == null) {
+            navigationView.setCheckedItem(0);
+        }
 
     }
 
@@ -98,35 +104,58 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
     }
 
+
     private void initNavDrawer() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         placesFragment = new PlacesFragment();
-        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle); //TODO check why deprecated
-        toggle.syncState();
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+
+        drawer.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        //mDrawerList.setOnClickListener(new DrawerItemClickListener());
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                drawer,         /* DrawerLayout object */
+                toolbar,
+                R.string.navigation_drawer_open,  /* "open drawer" description for accessibility */
+                R.string.navigation_drawer_close  /* "close drawer" description for accessibility */
+        ) {
+            public void onDrawerClosed(View view) {
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        drawer.addDrawerListener(mDrawerToggle);
 
         fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.add(R.id.container, placesFragment);
         transaction.commit();
-        // TODO поставить маркер того, что айтем выбран
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
+                item.setChecked(true);
                 switch (id) {
-                    case R.id.nav_main:
+                    case R.id.action_nav_main:
                         fragment = placesFragment;
                         break;
-                    case R.id.nav_map:
+                    case R.id.action_nav_map:
                         fragment = new GameMapFragment();
-                        break;
-                    case R.id.nav_settings:
-                  //      Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-                     //   startActivity(intent);
                         break;
                 }
 
@@ -137,6 +166,20 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 return true;
             }
         });
+    }
+
+
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     private Retrofit retrofitBuilder() {
@@ -209,13 +252,22 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        switch (item.getItemId()) {
             case R.id.action_update:
                 makeRequestToWiki(currentLatLng);
+                return true;
+            case R.id.action_settings:
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
                 return true;
 
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
 
